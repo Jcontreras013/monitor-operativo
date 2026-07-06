104c0a7955592b54660c9647b3355100ee8c7f56
Jcontreras013 <118787598+Jcontreras013@users.noreply.github.com>
2026-07-06 16:15:08 -0600

    app.py

--- app.py
+++ app.py
@@ -1449,7 +1449,7 @@ def main():
             st.markdown("---")
             with st.expander("Ver Lista Detallada"): st.dataframe(df_cerradas_espejo[['NUM', 'TECNICO', 'ACTIVIDAD', 'TIEMPO_REAL', 'COMENTARIO']], hide_index=True, use_container_width=True)
 
-with tab_materiales:
+        with tab_materiales:
             st.subheader("🔌 Control de Materiales e Inventario (Equipos y Acometidas)")
             st.caption("Reporte de cambios de equipos terminales (ONT/ONU/CPE) y reemplazos de cable acometida (Drop) enfocado exclusivamente en órdenes de Soporte y CEQUI.")
             
